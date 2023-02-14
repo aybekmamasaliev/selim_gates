216b272465cb69b4f43ef78f96e8fc5a0d473435
Arthur Badin <akherousin94@gmail.com>
2023-02-15 02:27:08 +0600

    refactor: styling changes

--- src/components/News/NewsList.js
+++ src/components/News/NewsList.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
 import s from './NewsList.module.css';
-import Button from '../Button/Button';
 import NewsItem from '../NewsItem/NewsItem';
-import Title from '../Title/Title';
 
 const NewsList = () => {
   return (
     <>
       <div className={s.news_block}>
-        {/* <p className={s.news_title}>Последние новости</p> */}
-        <Title>Последние новости</Title>
         <div className={s.news_list}>
           <NewsItem />
           <NewsItem />
           <NewsItem />
         </div>
-        <Button isSecondary>Все новости</Button>
       </div>
     </>
   );
