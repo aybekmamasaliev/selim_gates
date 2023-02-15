9a6e9993902377e80ef18554222b067f3537ea62
Arthur Badin <akherousin94@gmail.com>
2023-02-16 02:48:07 +0600

    feat: add Map component and add it to the Footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Form from './components/Form/Form';
-import NewsList from './components/News/NewsList';
-import OurServices from './components/OurServices/OurServices';
-import ReviewList from './components/ReviewList/ReviewList';
-import OurWorks from "./components/OurWorks/OurWorks"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
 
 function App() {
   return (
-    <div>
-      <Form/>
-      <OurServices />
-      <NewsList/>
-      <ReviewList />
-      <OurWorks/>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
