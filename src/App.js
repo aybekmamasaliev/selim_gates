6bb97cede0507ac9c4a54ca8aea2ef7cf06c22a1
aybek <aybek.mamasaliev@bk.ru>
2023-02-15 20:55:22 +0600

    feat: changet to arrow function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import MainPage from "./pages/MainPage/MainPage"
 
-function App() {
+const App=()=>{
   return (
     <BrowserRouter>
     <Routes>
