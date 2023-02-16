c7fee3adae1a7199b44356a555bdeffeccd490de
Arthur Badin <akherousin94@gmail.com>
2023-02-16 23:59:01 +0600

    feat: add route to services page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import MainPage from "./pages/MainPage/MainPage"
+import MainPage from './pages/MainPage/MainPage';
+import GatesPage from './pages/GatesPage/GatesPage';
 
-const App=()=>{
+const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/home" element={<MainPage />} />
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/home" element={<MainPage />} />
+        <Route path="/services" element={<GatesPage />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
