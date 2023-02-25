7bbc550956632fbc89a409ee5ce0ad9985d3e8f1
Arthur Badin <akherousin94@gmail.com>
2023-02-26 00:31:40 +0600

    refactor: change component name

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
-import Services from './pages/Services/Services';
+import ServicesPage from './pages/ServicesPage/ServicesPage';
 import GatesPage from './pages/GatesPage/GatesPage';
 import AllNews from './pages/AllNews/AllNews';
 import GalleryPage from './pages/GalleryPage/GalleryPage.js';
 // import Slider from './components/Slider/Slider';
 
-function App(){
+function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/home" element={<MainPage />} />
-          <Route path="/services" element={<Services />} />
+          <Route path="/services" element={<ServicesPage />} />
           <Route path="/news" element={<AllNews />} />
-           <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
           <Route path="/services/1" element={<GatesPage />} />
           {/* <Route path='/swiper' element={<Slider />} /> */}
         </Routes>
       </BrowserRouter>
     </>
   );
-};
+}
 
 export default App;
